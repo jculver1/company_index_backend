4d54b2a34cc4e47e04fc9dcb902e740254f6edb4
Jessica Culver <jculver@us-l-1560.hsd1.co.comcast.net>
2021-03-21 17:31:46 -0600

    updated env

--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,12 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: 'process.env.DATABASE_URL' || 'postgres://whuvjccusymcvt:ac06f3dcd96bd172000ae7bf39da646eefe2a5c181dc40ddd82d1f73a34c76ec@ec2-54-198-252-9.compute-1.amazonaws.com:5432/d683etfl2mli5e'
+    connection: process.env.DATABASE_URL || 'postgres://whuvjccusymcvt:ac06f3dcd96bd172000ae7bf39da646eefe2a5c181dc40ddd82d1f73a34c76ec@ec2-54-198-252-9.compute-1.amazonaws.com:5432/d683etfl2mli5e'
+    },
+
+    development: {
+      client: 'pg',
+      connection: process.env.DATABASE_URL || 'localhost'
+    }
   }
 }
