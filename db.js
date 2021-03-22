d801be4f7fd519f4576ab02f6f8e4823ee89d02b
jculver1 <jculver1900@gmail.com>
2021-03-21 21:38:16 -0600

    update

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 
-const environment = process.env.NODE_ENV || 'development';
+const environment = process.env.NODE_ENV || 'production';
 const knexConfig = require('./knexfile')[environment]
 const db = require('knex')(knexConfig)
 
