4065026ce4a4edd4e876ab5741c5092acdd71108
Jessica Culver <jculver@us-l-1560.hsd1.co.comcast.net>
2021-03-21 18:27:05 -0600

    update

--- knexfile.js
+++ knexfile.js
@@ -4,18 +4,6 @@ module.exports = {
 
   production: {
     client: 'pg',
-    database: {
-      type: 'postgres',
-      host: '3.82.213.89',
-      username: 'whuvjccusymcvt',
-      database: 'd683etfl2mli5e',
-      ssl: true,
-      extra: {
-        ssl: {
-          rejectUnauthorized: false
-        }
-      }
-    }
-    // connection: process.env.DATABASE_URL || 'postgres://whuvjccusymcvt:ac06f3dcd96bd172000ae7bf39da646eefe2a5c181dc40ddd82d1f73a34c76ec@ec2-54-198-252-9.compute-1.amazonaws.com:5432/d683etfl2mli5e'
+    connection: process.env.DATABASE_URL || 'postgres://whuvjccusymcvt:ac06f3dcd96bd172000ae7bf39da646eefe2a5c181dc40ddd82d1f73a34c76ec@ec2-54-198-252-9.compute-1.amazonaws.com:5432/d683etfl2mli5e?ssl=true'
   }
 }
