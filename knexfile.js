d7c00bcfc586a6b60da4b5d36840ab37912488fa
Jessica Culver <jculver@us-l-1560.hsd1.co.comcast.net>
2021-03-21 23:31:01 -0600

    sdf

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL || 'localhost'
+    connection: process.env.DATABASE_URL || 'postgres://kvrxfipqczozrn:025e816e350661858171e4aa7c28b05c747df389d926962713838e7b19b2357c@ec2-3-91-127-228.compute-1.amazonaws.com:5432/df5pphbekve3r6'
   },
 
   production: {
