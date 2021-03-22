deeb3816ab5154c0b089ef5fdc17f700a2b3e38f
Jessica Culver <jculver@us-l-1560.hsd1.co.comcast.net>
2021-03-21 20:39:45 -0600

    up

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL || 'postgres://kvrxfipqczozrn:025e816e350661858171e4aa7c28b05c747df389d926962713838e7b19b2357c@ec2-3-91-127-228.compute-1.amazonaws.com:5432/df5pphbekve3r6',
-    ssl: { rejectUnauthorized: false }
+    // ssl: { rejectUnauthorized: false }
   },
 
   development: {
