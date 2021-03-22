851a14d680eaa761bfbe29bfb77a61326b4228a1
Jessica Culver <jculver@us-l-1560.hsd1.co.comcast.net>
2021-03-21 23:18:49 -0600

    updated config

--- db.js
+++ db.js
@@ -1,8 +1,6 @@
 
-const environment = process.env.NODE_ENV || 'production';
+const environment = process.env.NODE_ENV || 'development'
 const knexConfig = require('./knexfile')[environment]
 const db = require('knex')(knexConfig)
-
-
 
 module.exports = db
--- knexfile.js
+++ knexfile.js
@@ -2,10 +2,10 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL 
+    connection: process.env.DATABASE_URL || 'localhost'
   },
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL 
+    connection: process.env.DATABASE_URL || 'postgres://kvrxfipqczozrn:025e816e350661858171e4aa7c28b05c747df389d926962713838e7b19b2357c@ec2-3-91-127-228.compute-1.amazonaws.com:5432/df5pphbekve3r6'
   }
