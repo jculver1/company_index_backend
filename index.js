cf64d1d6a66b40b25e67cd5ca1aa1e1103a4233f
Jessica Culver <jculver@us-l-1560.hsd1.co.comcast.net>
2021-03-21 23:30:07 -0600

    sdf

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 3001
 const cors = require('cors')
 const parser = require('body-parser')
 const db = require('./db')
