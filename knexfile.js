682ccb4e535f67159c9280e16a8e4c77ab0ca01a
jculver1 <jculver1900@gmail.com>
2021-03-21 21:21:00 -0600

    update

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,15 @@
 module.exports = {
 
+  development: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/eatit'
+  },
+
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL || 'postgres://csmtumindlpyqz:7eb96abd5857e7d2326840c604255f87c1ba6f96a7f1970acc700ae266ed7355@ec2-54-164-22-242.compute-1.amazonaws.com:5432/d2oen615t07pgf'
-  },
-
-  development: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL || 'localhost'
   }
 
 }
+
+
